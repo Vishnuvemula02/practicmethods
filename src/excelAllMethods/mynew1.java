efb58907aee7d65941292cc57e6075ddf467da7c
Dell <Dell@DESKTOP-EV76MD6>
2024-09-27 16:50:47 +0530

    new2

---
+++ src/excelAllMethods/mynew1.java
@@ -0,0 +1,32 @@
+package excelAllMethods;
+
+public class mynew1 {
+
+
+	public static void add() {
+		int a=66,b=99,c;
+		c=a+b;
+	System.out.println(c);
+	
+	}
+	public static void sub() {
+		int a=89,b=7,c;
+		c=a-b;
+	System.out.println(c);
+	
+	}
+	public static void mut() {
+		int a=88,b=77,c;
+		c=a*b;
+	System.out.println(c);
+	
+	}
+	public static void div() {
+		int a=88,b=77,c;
+		c=a/b;
+	System.out.println(c);
+	
+	}
+
+			
+}
